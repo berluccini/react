6bfed9981334155d57d02388bf471a174c903a11
berluccini <148586794+berluccini@users.noreply.github.com>
2024-01-16 08:10:34 -0300

    react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import Home from './assets/pages/Home/Home'
+
+function App() {
+  
+  return (
+<>
+<Home/>
+
+</>
+      
+  )
+}
+export default App
---
+++ src/assets/pages/Home/Home.tsx
@@ -0,0 +1,42 @@
+
+
+function Home (){
+
+    return(
+        <>
+        <div style={{
+            width: "100vw",
+            display:"flex",
+            justifyContent: "center"
+            }}>
+            <div style={{
+                width:"80vw",
+                display:"flex",
+                flexDirection:"column",
+                alignItems:"center"
+            }}>
+                <h2>seja bem vindo</h2>
+                
+                <p>expresse aqui seus sentimentos</p>
+                <div>
+
+
+                <img src="" alt="" />
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+        
+        
+        
+        
+        </>
+
+    )
+}
+
+export default Home
